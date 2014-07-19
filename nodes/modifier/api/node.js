048de382eef6b3f694862bd462ea1464a39ae79f
Rob Halff <rob.halff@gmail.com>
2014-07-20 01:12:11 +0200

    move options to first level

--- nodes/modifier/api/node.js
+++ nodes/modifier/api/node.js
@@ -1,4 +1,9 @@
 output = function() {
-  cb( { modifier: new modifier(input.options) });
+  var opts =  { origin: input.origin, transform: input.transform };
+  if(input.size) opts.size = size;
+  if(input.opacity) opts.opacity = size;
+  cb( {
+    modifier: new modifier(opts)
+  });
   done();
 };
