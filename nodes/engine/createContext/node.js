419b5990f49fae57fa3834e84138ddad05d6612c
Rob Halff <rob.halff@gmail.com>
2014-08-04 07:41:43 +0200

    adding start functionality

--- nodes/engine/createContext/node.js
+++ nodes/engine/createContext/node.js
@@ -1,9 +1,8 @@
-// must be called outside, to have any change.
-// will still be buggy though
-state = engine.createContext();
+on.start = function() {
+  state = engine.createContext();
+  output( { context: state });
+};
 
 on.input.el = function() {
   state.migrate(data);
 };
-
-output = { context: state };
