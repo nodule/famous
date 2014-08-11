8ff8a07d0c296cd934c293ee00e7b378ef209d6d
Rob Halff <rob.halff@gmail.com>
2014-08-11 19:23:02 +0200

    bind to make sure

--- nodes/surface/on/node.js
+++ nodes/surface/on/node.js
@@ -2,7 +2,7 @@ output = function() {
 
   input.surface.on(input.type, function() {
      output({ event: {} });
-  });
+  }.bind(input.surface));
 
   output({ surface: input.surface });
   done();
