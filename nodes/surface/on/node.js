7a2b8ef03c7029cd0ea38e368657b3cd6430a87d
Rob Halff <rob.halff@gmail.com>
2014-08-12 17:00:48 +0200

    finally

--- nodes/surface/on/node.js
+++ nodes/surface/on/node.js
@@ -1,9 +1,8 @@
 output = function(cb) {
-  console.log('SO!', input, cb, output, data);
   input.surface.on(input.event, function() {
     cb({ event: {} });
   }.bind(input.surface));
 
   cb({ surface: input.surface });
+  done();
 };
-console.log('ON LOADED', output);
